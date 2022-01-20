13ec437cc697920004db1c74343513df2f87a4b5
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-20 15:45:11 +0530

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; DigiCatalog</Col>
+          <Col className='text-center py-3'>Copyright &copy; SaiPrasad</Col>
         </Row>
       </Container>
     </footer>
