a9129c3592cfad005b8b178b227ffc9b8df5ec50
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-20 16:39:56 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect >
         <Container>
         <LinkContainer to='/'>
-        <Navbar.Brand>Bookisfy</Navbar.Brand>
+        <Navbar.Brand>Booksify</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
